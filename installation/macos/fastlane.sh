5f358e69716f3e9f27edb23142f1dccfecdfcc9d
kujyp <swe.jaeyoungpark@navercorp.com>
2019-11-07 01:00:06 +0900

    M  installation/macos/fastlane.sh

--- installation/macos/fastlane.sh
+++ installation/macos/fastlane.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-# brew cask install fastlane # 2.28.3
-brew install ruby
-echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/.zshrc
-source ~/.zshrc
-
-sudo gem install fastlane -NV
-# signet requires Ruby version >= 2.4.0.
 
 if [[ "$(basename $SHELL)" == "bash" ]]; then
     rcfile=.bash_profile
-elif [[ "$(basename $SHELL)" == "zsh" ]];
-    rcfile=
+elif [[ "$(basename $SHELL)" == "zsh" ]]; then
+    rcfile=.zshrc
+fi
 
-echo -e "# fastlane\nexport PATH=\"\$HOME/.fastlane/bin:\$PATH\"\n" >> ~/.zshrc
-source ~/.zshrc
+
+brew install ruby
+echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/${rcfile}
+source ~/${rcfile}
+
+
+sudo gem install fastlane -NV
+fastlane_bin_path=$(dirname $(find /usr/local -name fastlane 2> /dev/null | grep '/bin/fastlane' | grep -v 'gems/fastlane' | head -n1))
+echo -e "# fastlane\nexport PATH=\"$fastlane_bin_path:\$PATH\"\n" >> ~/${rcfile}
+source ~/${rcfile}
 fastlane update_fastlane
