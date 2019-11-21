eecbd15bdcc9627326de33cd6d5ee61e5121cdb1
kujyp <swe.jaeyoungpark@navercorp.com>
2019-11-21 20:00:04 +0900

    A  installation/macos/pyenv.sh

---
+++ installation/macos/pyenv.sh
@@ -0,0 +1,15 @@
+
+
+brew install openssl readline sqlite3 xz zlib
+curl https://pyenv.run | bash
+
+echo "export PATH=~/.pyenv/bin:\$PATH
+eval \"\$(pyenv init -)\"
+eval \"\$(pyenv virtualenv-init -)\"
+" >> ~/.zshrc
+source ~/.zshrc
+
+# pyenv install --list | grep 3.6
+pyenv install --skip-existing 3.6.9
+pyenv install --skip-existing 3.7.5
+pyenv install --skip-existing 2.7.17
